36cd7e9ffc1dc679e7bc6fc4fb094a158089ce33
Yekai Yu <yekaiyu94@gmail.com>
2021-07-20 12:30:23 -0500

    q3 to dynamodb

--- pyspark/Task.py
+++ pyspark/Task.py
@@ -49,7 +49,7 @@ df = spark \
   .load()
 
 df = df.selectExpr("CAST(value AS STRING)") \
-       .select(from_json(col('value'), schema).alias("data")).select("data.*") \
+       .select(from_json(col('value'), schema).alias("data")).select("data.*")
 
 '''
 # =============== Q1.2 ===============
@@ -65,7 +65,6 @@ q1dot3df = df.select(col("UniqueCarrier"), col("ArrDelay")) \
              .orderBy("AVG(ArrDelay)") \
              .limit(11)
 
-'''
 # =============== Q2.1 ===============
 q2dot1df_SRQ = df.select("Origin", "UniqueCarrier", "DepDelay") \
                 .where(col("Origin") == "SRQ") \
@@ -168,97 +167,107 @@ q2dot3df_4 = df.select("Origin", "Dest", "UniqueCarrier", "ArrDelay") \
                 .orderBy(col("AVG(ArrDelay)").asc()) \
                 .limit(10)
 '''
-# =============== Q3.2 ===============
-df_2008 = df
-df_2008 = df_2008.withColumn("FlightDateUniform", 
-                            to_date(concat(lpad(col("Month"),2,'0'), 
-                                           lpad(col("DayofMonth"),2,'0'), 
-                                           col("Year")), 
-                                    "MMddyyyy"))
-df_2008 = df_2008.filter(df_2008["FlightDateUniform"].between("2008-01-01","2008-12-31"))
-df_2008 = df_2008.withColumn("CRSDepTimeUniform", 
-                            to_timestamp(concat(col("FlightDateUniform"), 
-                                                lit(" "), 
-                                                lpad(col("CRSDepTime"),4,'0')), 
-                                        "yyyy-MM-dd HHmm"))
+def get_dynamodb():
+  return boto3.resource('dynamodb', region_name = "us-east-1")
 
-print("first leg, before 12:00PM")
-df_X_Y = df_2008.filter(concat(lpad(hour(col("CRSDepTimeUniform")),2,'0'), 
-                               lpad(minute(col("CRSDepTimeUniform")), 2, '0'))
-                        .between(lit("0000"), lit("1200")))
-df_X_Y = df_X_Y.alias("XY")
-# individual date arrival performance
-df_X_Y_groupby = df_X_Y.groupBy("Origin", "Dest", "FlightDateUniform") \
-                       .agg({"ArrDelay": "min"})
-df_X_Y_groupby = df_X_Y_groupby.alias("XYGroupBy")
-X_Y_cond = [(col("XY.Origin") == col("XYGroupBy.Origin")), 
-            (col("XY.Dest") == col("XYGroupBy.Dest")), 
-            (col("XY.FlightDateUniform") == col("XYGroupBy.FlightDateUniform")), 
-            (col("XY.ArrDelay") == col("XYGroupBy.min(ArrDelay)"))]
-df_X_Y = df_X_Y.join(df_X_Y_groupby, X_Y_cond, "inner")
-df_X_Y = df_X_Y.drop("min(ArrDelay)")
+def foreach_batch_function(df, epoch_id):
+    q3_2_table_name = "q3.2"
+    # =============== Q3.2 ===============
+    df_2008 = df
+    df_2008 = df_2008.withColumn("FlightDateUniform", 
+                                to_date(concat(lpad(col("Month"),2,'0'), 
+                                            lpad(col("DayofMonth"),2,'0'), 
+                                            col("Year")), 
+                                        "MMddyyyy"))
+    df_2008 = df_2008.filter(df_2008["FlightDateUniform"].between("2008-01-01","2008-12-31"))
+    df_2008 = df_2008.withColumn("CRSDepTimeUniform", 
+                                to_timestamp(concat(col("FlightDateUniform"), 
+                                                    lit(" "), 
+                                                    lpad(col("CRSDepTime"),4,'0')), 
+                                            "yyyy-MM-dd HHmm"))
 
-print("second leg, after 12:00PM")
-df_Y_Z = df_2008.filter(concat(lpad(hour(col("CRSDepTimeUniform")),2,'0'), 
-                               lpad(minute(col("CRSDepTimeUniform")), 2, '0'))
-                        .between(lit("1200"), lit("2400")))
-df_Y_Z = df_Y_Z.alias("YZ")
-# individual date arrival performance
-df_Y_Z_groupby = df_Y_Z.groupBy("Origin", "Dest", "FlightDateUniform").agg({"ArrDelay": "min"})
-df_Y_Z_groupby = df_Y_Z_groupby.alias("YZGroupBy")
-Y_Z_cond = [(col("YZ.Origin") == col("YZGroupBy.Origin")), 
-            (col("YZ.Dest") == col("YZGroupBy.Dest")), 
-            (col("YZ.FlightDateUniform") == col("YZGroupBy.FlightDateUniform")), 
-            (col("YZ.ArrDelay") == col("YZGroupBy.min(ArrDelay)"))]
-df_Y_Z = df_Y_Z.join(df_Y_Z_groupby, Y_Z_cond, "inner")
-df_Y_Z = df_Y_Z.drop("min(ArrDelay)")
+    print("first leg, before 12:00PM")
+    df_X_Y = df_2008.filter(concat(lpad(hour(col("CRSDepTimeUniform")),2,'0'), 
+                                lpad(minute(col("CRSDepTimeUniform")), 2, '0'))
+                            .between(lit("0000"), lit("1200")))
+    df_X_Y = df_X_Y.alias("XY")
+    # individual date arrival performance
+    df_X_Y_groupby = df_X_Y.groupBy("Origin", "Dest", "FlightDateUniform") \
+                        .agg({"ArrDelay": "min"})
+    df_X_Y_groupby = df_X_Y_groupby.alias("XYGroupBy")
+    X_Y_cond = [(col("XY.Origin") == col("XYGroupBy.Origin")), 
+                (col("XY.Dest") == col("XYGroupBy.Dest")), 
+                (col("XY.FlightDateUniform") == col("XYGroupBy.FlightDateUniform")), 
+                (col("XY.ArrDelay") == col("XYGroupBy.min(ArrDelay)"))]
+    df_X_Y = df_X_Y.join(df_X_Y_groupby, X_Y_cond, "inner")
+    df_X_Y = df_X_Y.drop("min(ArrDelay)")
 
-print("*** Joining XY & YZ ***")
-df_X_Y_Z = df_X_Y.join(df_Y_Z, col("XY.Dest") == col("YZ.Origin"), 'inner') \
-                 .where(datediff(col("YZ.FlightDateUniform"), 
-                                 col("XY.FlightDateUniform")) == 2)
+    print("second leg, after 12:00PM")
+    df_Y_Z = df_2008.filter(concat(lpad(hour(col("CRSDepTimeUniform")),2,'0'), 
+                                lpad(minute(col("CRSDepTimeUniform")), 2, '0'))
+                            .between(lit("1200"), lit("2400")))
+    df_Y_Z = df_Y_Z.alias("YZ")
+    # individual date arrival performance
+    df_Y_Z_groupby = df_Y_Z.groupBy("Origin", "Dest", "FlightDateUniform").agg({"ArrDelay": "min"})
+    df_Y_Z_groupby = df_Y_Z_groupby.alias("YZGroupBy")
+    Y_Z_cond = [(col("YZ.Origin") == col("YZGroupBy.Origin")), 
+                (col("YZ.Dest") == col("YZGroupBy.Dest")), 
+                (col("YZ.FlightDateUniform") == col("YZGroupBy.FlightDateUniform")), 
+                (col("YZ.ArrDelay") == col("YZGroupBy.min(ArrDelay)"))]
+    df_Y_Z = df_Y_Z.join(df_Y_Z_groupby, Y_Z_cond, "inner")
+    df_Y_Z = df_Y_Z.drop("min(ArrDelay)")
 
-print("*** Outputing ***")
-df_X_Y_Z_select = df_X_Y_Z.select(col("XY.Origin").alias("Origin 1"), 
-                                  col("XY.Dest").alias("Destination 1"), 
-                                  concat(col("XY.UniqueCarrier"), 
-                                         lit(" "), 
-                                         col("XY.FlightNum"))
-                                         .alias("Airline/Flight Number 1"), 
-                                  date_format(col("XY.CRSDepTimeUniform"), "HH:mm dd/MM/yyyy")
-                                            .alias("Sched Depart 1"), 
-                                  col("XY.ArrDelay").alias("Arrival Delay 1"), 
-                                  col("YZ.Origin").alias("Origin 2"), 
-                                  col("YZ.Dest").alias("Destination 2"), 
-                                  concat(col("YZ.UniqueCarrier"), 
-                                         lit(" "), 
-                                         col("YZ.FlightNum"))
-                                         .alias("Airline/Flight Number 2"), 
-                                  date_format(col("YZ.CRSDepTimeUniform"), "HH:mm dd/MM/yyyy")
-                                            .alias("Sched Depart 2"), 
-                                  col("YZ.ArrDelay").alias("Arrival Delay 2"))
+    print("*** Joining XY & YZ ***")
+    df_X_Y_Z = df_X_Y.join(df_Y_Z, col("XY.Dest") == col("YZ.Origin"), 'inner') \
+                    .where(datediff(col("YZ.FlightDateUniform"), 
+                                    col("XY.FlightDateUniform")) == 2)
 
-q3dot2_df1 = df_X_Y_Z_select.where(col("XY.Origin") == "BOS" & col("XY.Dest") == "ATL" & col("YZ.Dest") == "LAX" & col("Sched Depart 1").like("%2008-04-03%"))
-q3dot2_df2 = df_X_Y_Z_select.where(col("XY.Origin") == "PHX" & col("XY.Dest") == "JFK" & col("YZ.Dest") == "MSP" & col("Sched Depart 1").like("%2008-09-07%"))
-q3dot2_df3 = df_X_Y_Z_select.where(col("XY.Origin") == "DFW" & col("XY.Dest") == "STL" & col("YZ.Dest") == "ORD" & col("Sched Depart 1").like("%2008-01-24%"))
-q3dot2_df4 = df_X_Y_Z_select.where(col("XY.Origin") == "LAX" & col("XY.Dest") == "MIA" & col("YZ.Dest") == "LAX" & col("Sched Depart 1").like("%2008-05-16%"))
+    print("*** Outputing ***")
+    df_X_Y_Z_select = df_X_Y_Z.select(col("XY.Origin").alias("Origin 1"), 
+                                    col("XY.Dest").alias("Destination 1"), 
+                                    concat(col("XY.UniqueCarrier"), 
+                                            lit(" "), 
+                                            col("XY.FlightNum"))
+                                            .alias("Airline/Flight Number 1"), 
+                                    date_format(col("XY.CRSDepTimeUniform"), "HH:mm dd/MM/yyyy")
+                                                .alias("Sched Depart 1"), 
+                                    col("XY.ArrDelay").alias("Arrival Delay 1"), 
+                                    col("YZ.Origin").alias("Origin 2"), 
+                                    col("YZ.Dest").alias("Destination 2"), 
+                                    concat(col("YZ.UniqueCarrier"), 
+                                            lit(" "), 
+                                            col("YZ.FlightNum"))
+                                            .alias("Airline/Flight Number 2"), 
+                                    date_format(col("YZ.CRSDepTimeUniform"), "HH:mm dd/MM/yyyy")
+                                                .alias("Sched Depart 2"), 
+                                    col("YZ.ArrDelay").alias("Arrival Delay 2"))
+
+    q3dot2_df1 = df_X_Y_Z_select.where((col("XY.Origin") == "BOS") & (col("XY.Dest") == "ATL") & (col("YZ.Dest") == "LAX") & col("Sched Depart 1").like("%2008-04-03%"))
+    print("Compelete query, writing to dynamo")
+    get_dynamodb().Table(q3_2_table_name).put_item(
+        Item = { 'origin-tran-dest-date': str(q3dot2_df1['Origin 1']) + "-" + str(q3dot2_df1['Destination 1'] + "-" + str(q3dot2_df1["Destination 2"]) + "-" + str(q3dot2_df1["Sched Depart 1"]) + "-" + str(q3dot2_df1["Sched Depart 2"])), 
+                 'totalArrDelay': str(q3dot2_df1['Arrival Delay 1']) + "," + str(q3dot2_df1['Arrival Delay 2']) })
+    
+    # q3dot2_df2 = df_X_Y_Z_select.where((col("XY.Origin") == "PHX") & (col("XY.Dest") == "JFK") & (col("YZ.Dest") == "MSP") & col("Sched Depart 1").like("%2008-09-07%"))
+    # q3dot2_df3 = df_X_Y_Z_select.where((col("XY.Origin") == "DFW") & (col("XY.Dest") == "STL") & (col("YZ.Dest") == "ORD") & col("Sched Depart 1").like("%2008-01-24%"))
+    # q3dot2_df4 = df_X_Y_Z_select.where((col("XY.Origin") == "LAX") & (col("XY.Dest") == "MIA") & (col("YZ.Dest") == "LAX") & col("Sched Depart 1").like("%2008-05-16%"))
+
+
 
 '''
-
 q2_1_table_name = "q2.1.0"
 q2_2_table_name = "q2.2.0"
 q2_3_table_name = "q2.3.0"
 
-def get_dynamodb():
-  return boto3.resource('dynamodb', region_name = "us-east-1")
+
 
 class Q2_1_SendToDynamoDB_ForeachWriter:
-  '''
-  Class to send a set of rows to DynamoDB.
-  When used with `foreach`, copies of this class is going to be used to write
-  multiple rows in the executor. See the python docs for `DataStreamWriter.foreach`
-  for more details.
-  '''
+
+#   Class to send a set of rows to DynamoDB.
+#   When used with `foreach`, copies of this class is going to be used to write
+#   multiple rows in the executor. See the python docs for `DataStreamWriter.foreach`
+#   for more details.
+
 
   def open(self, partition_id, epoch_id):
     # This is called first when preparing to send multiple rows.
@@ -284,12 +293,12 @@ class Q2_1_SendToDynamoDB_ForeachWriter:
       raise err
 
 class Q2_2_SendToDynamoDB_ForeachWriter:
-  '''
-  Class to send a set of rows to DynamoDB.
-  When used with `foreach`, copies of this class is going to be used to write
-  multiple rows in the executor. See the python docs for `DataStreamWriter.foreach`
-  for more details.
-  '''
+
+#   Class to send a set of rows to DynamoDB.
+#   When used with `foreach`, copies of this class is going to be used to write
+#   multiple rows in the executor. See the python docs for `DataStreamWriter.foreach`
+#   for more details.
+
 
   def open(self, partition_id, epoch_id):
     # This is called first when preparing to send multiple rows.
@@ -314,12 +323,12 @@ class Q2_2_SendToDynamoDB_ForeachWriter:
       raise err
 
 class Q2_3_SendToDynamoDB_ForeachWriter:
-  '''
-  Class to send a set of rows to DynamoDB.
-  When used with `foreach`, copies of this class is going to be used to write
-  multiple rows in the executor. See the python docs for `DataStreamWriter.foreach`
-  for more details.
-  '''
+
+#   Class to send a set of rows to DynamoDB.
+#   When used with `foreach`, copies of this class is going to be used to write
+#   multiple rows in the executor. See the python docs for `DataStreamWriter.foreach`
+#   for more details.
+
 
   def open(self, partition_id, epoch_id):
     # This is called first when preparing to send multiple rows.
@@ -343,27 +352,36 @@ class Q2_3_SendToDynamoDB_ForeachWriter:
     if err:
       raise err
 
+'''
 
-def execute(df_, t):
+# def execute(df_, t):
+#     q = (
+#         df_.writeStream\
+#             .outputMode("complete") \
+#             .option("truncate", "false")\
+#             .format("console") \
+#             .start()
+#     )
+#     stop_stream_query(q, t)
+
+# def execute_q2(df_, t, writer):
+#     q = (
+#         df_.writeStream\
+#             .foreach(writer) \
+#             .outputMode("complete") \
+#             .start()
+#     )
+#     stop_stream_query(q, t)
+#     print("--- Done ---")
+
+def execute_q3(df_, t):
     q = (
         df_.writeStream\
-            .outputMode("complete") \
-            .option("truncate", "false")\
-            .format("console") \
+            .outputMode("append") \
+            .foreachBatch(foreach_batch_function) \
             .start()
     )
     stop_stream_query(q, t)
-
-def execute_q2(df_, t, writer):
-    q = (
-        df_.writeStream\
-            .foreach(writer) \
-            .outputMode("complete") \
-            .start()
-    )
-    stop_stream_query(q, t)
-    print("--- Done ---")
-
 
 # =============== Execution ===============
 interval = 5
@@ -371,25 +389,24 @@ interval = 5
 #execute(q1dot2df, interval)
 # execute(q1dot3df, interval)
 
-execute_q2(q2dot1df_SRQ, interval, Q2_1_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot1df_CMH, interval, Q2_1_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot1df_JFK, interval, Q2_1_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot1df_SEA, interval, Q2_1_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot1df_BOS, interval, Q2_1_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot1df_SRQ, interval, Q2_1_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot1df_CMH, interval, Q2_1_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot1df_JFK, interval, Q2_1_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot1df_SEA, interval, Q2_1_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot1df_BOS, interval, Q2_1_SendToDynamoDB_ForeachWriter())
 
-execute_q2(q2dot2df_SRQ, interval, Q2_2_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot2df_CMH, interval, Q2_2_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot2df_JFK, interval, Q2_2_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot2df_SEA, interval, Q2_2_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot2df_BOS, interval, Q2_2_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot2df_SRQ, interval, Q2_2_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot2df_CMH, interval, Q2_2_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot2df_JFK, interval, Q2_2_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot2df_SEA, interval, Q2_2_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot2df_BOS, interval, Q2_2_SendToDynamoDB_ForeachWriter())
 
-execute_q2(q2dot3df_1, interval, Q2_3_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot3df_2, interval, Q2_3_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot3df_3, interval, Q2_3_SendToDynamoDB_ForeachWriter())
-execute_q2(q2dot3df_4, interval, Q2_3_SendToDynamoDB_ForeachWriter())
-'''
-execute(q3dot2_df1, interval)
-execute(q3dot2_df2, interval)
-execute(q3dot2_df3, interval)
-execute(q3dot2_df4, interval)
-'''
+# execute_q2(q2dot3df_1, interval, Q2_3_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot3df_2, interval, Q2_3_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot3df_3, interval, Q2_3_SendToDynamoDB_ForeachWriter())
+# execute_q2(q2dot3df_4, interval, Q2_3_SendToDynamoDB_ForeachWriter())
+
+execute_q3(df, interval)
+# execute_q3(q3dot2_df2, interval)
+# execute_q3(q3dot2_df3, interval)
+# execute_q3(q3dot2_df4, interval)
